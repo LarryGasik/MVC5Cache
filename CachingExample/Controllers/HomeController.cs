9e5b5b2c62aa7935b4f344f39a6a299fbbb5265b
Larry Gasik <larry@lgproductions.us>
2019-05-04 14:37:32 -0500

    Set up the server language for Culture Info.

--- CachingExample/Controllers/HomeController.cs
+++ CachingExample/Controllers/HomeController.cs
@@ -30,13 +30,11 @@ namespace CachingExample.Controllers
         {
             IndexViewModel model = new IndexViewModel();
 
-            //Todo: I need to figure out how the Culture Info works. I want this
-            //      to grab the Requesting language from the browser, and then display
-            //      currency in their format. For example, in the US use $ and . but in
-            //      france use € and ,
-            //      DateTime also needs to be formatted correctly as well. 
-            //CultureInfo culture = new CultureInfo(Request.UserLanguages[0]);
-            //model.FormattedCurrency = (3.5).ToString("C", culture);
+            //Todo: We need to figure out how to generate a cultureInfo when
+            //      the server does not have said language. We need to make
+            //      sure that we loop through each of the languages as well.
+            CultureInfo culture = new CultureInfo(Request.UserLanguages[0]);
+            model.FormattedCurrency = (3.5).ToString("C", culture);
 
             model.FormattedCurrency = (3.5).ToString("C");
             model.FormattedTime = _timeService.DateTimeNow().ToString();
@@ -51,8 +49,10 @@ namespace CachingExample.Controllers
         public ActionResult Product()
         {
 
+
             IndexViewModel model = new IndexViewModel();
-            model.FormattedCurrency = (3.5).ToString("C");
+            CultureInfo culture = new CultureInfo(Request.UserLanguages[0]);
+            model.FormattedCurrency = (3.5).ToString("C", culture);
             model.FormattedTime = _timeService.DateTimeNow().ToString();
             return View(model);
         }
